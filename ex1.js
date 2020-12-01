195839f993cdfd21836d86fdb6ddb2b2638b028f
Luke <llogan382@gmail.com>
2020-12-01 16:25:41 -0500

    Expand upon our error messages

--- ex1.js
+++ ex1.js
@@ -9,18 +9,37 @@ var args = require("minimist")(process.argv.slice(2), {
     string: ["file"]
 });
 
-console.log(args);
+
+if (args.help){
+    printHelp();
+}
+
+else if (args.file){
+    console.log(args.file);
+}
+else {
+    error("Incorrect usage.", true);
+}
 
 
+
+
+function error(msg,includeHelp = false){
+    console.error(msg);
+    if(includeHelp){
+        console.log("");
+        printHelp();
+    }
+}
 
 
 // Always add helper  error messages
 
 function printHelp(){
-    console.log("ex1 usage: ")
-    console.log("   ex1.js --help");
-    console.log("")
+    console.log("ex1 usage: ");
+    console.log("   ex1.js --file={FILENAME");
+    console.log("");
     console.log("--help                     print this help")
-    console.log("")
-
+    console.log("--file={FILENAME}      process the file");
+    console.log("");
 }
